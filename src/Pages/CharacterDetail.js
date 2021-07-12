fbb1e429c550d684bc6fe854c261728ceb6b60c1
Yunus İNAN <yns.inan1660@gmail.com>
2021-07-12 15:34:35 +0300

    Detail Page Fixed

--- src/Pages/CharacterDetail.js
+++ src/Pages/CharacterDetail.js
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router";
 import { getSelectedCharacter } from '../Utils/Fetch'
 import { Link } from "react-router-dom";
+import { IoChevronBackCircle } from "react-icons/io5"
+
 const CharacterDetail = () => {
+
     const { id } = useParams();
+
 
     const [character, setCharacter] = useState({
         name: "",
@@ -16,6 +20,8 @@ const CharacterDetail = () => {
     });
 
     let count = 0;
+
+
     async function getSelectChar(id) {
         const data = await getSelectedCharacter(id);
         setCharacter(data);
@@ -26,14 +32,11 @@ const CharacterDetail = () => {
 
     return (
         <div className="selected-character">
-
-            <Link to="/" className="back-link">
-                Go Back
-            </Link>
-
-
-
-
+            <div className="back-link" >
+                <Link to="/" >
+                    <IoChevronBackCircle size={50} className="icon" />
+                </Link>
+            </div>
             <div className="character-container">
                 <div className="img-container">
                     <img src={character.image} alt={character.name} />
@@ -56,7 +59,13 @@ const CharacterDetail = () => {
                             <strong>Origin:</strong> {character.origin.name}
                         </li>
                         <li>
-                            <strong>Episodes:</strong> {character.episode.length}
+
+                            <strong>Episodes:</strong> {character.episode.slice(0, 5).map((element, index) => {
+                                return (
+                                    <p>{index + 1}-{element.substring(32, 42)}</p>
+                                )
+                            })}
+
                         </li>
                     </ul>
                 </div>
